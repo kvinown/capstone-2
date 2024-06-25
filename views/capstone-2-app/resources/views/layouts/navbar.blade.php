caab049cdcac1472c8b39d4e37177ed97f91d3dc
JosephAdiwiguna1 <113487450+JosephAdiwiguna1@users.noreply.github.com>
2024-06-25 13:53:21 +0700

    Changes by Kevin Owen

--- views/capstone-2-app/resources/views/layouts/navbar.blade.php
+++ views/capstone-2-app/resources/views/layouts/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light shadow">
     <div class="container d-flex justify-content-between align-items-center">
 
-        <a class="navbar-brand text-success logo h1 align-self-center" href="{{ route('home-index') }}">
+        <a class="navbar-brand text-success logo h1 align-self-center" href="{{route('home-index')}}">
             <img src="assets/img/Maranatha_Logo.png" width="100px">
         </a>
 
@@ -12,20 +12,29 @@
         <div class="align-self-center collapse navbar-collapse flex-fill  d-lg-flex justify-content-lg-between" id="templatemo_main_nav">
             <div class="flex-fill">
                 <ul class="nav navbar-nav d-flex justify-content-between mx-lg-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{route('pengajuanBeasiswa.create')}}">Daftar Sekarang</a>
-                    </li>
+                    @if(auth()->user()->role_id == '4')
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{route('pengajuanBeasiswa.create')}}">Daftar Sekarang</a>
+                        </li>
+                    @endif
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Menu
                         </a>
                         <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="{{route('dokumenBeasiswa.index')}}">Hasil Pengajuan</a></li>
-                            <li><a class="dropdown-item" href="{{route('fakultas.index')}}">Fakultas</a></li>
-                            <li><a class="dropdown-item" href="{{route('programStudi.index')}}">Program Studi</a></li>
-                            <li><a class="dropdown-item" href="{{route('jenisBeasiswa.index')}}">Jenis Beasiswa</a></li>
-                            <li><a class="dropdown-item" href="{{route('users.index')}}">Users</a></li>
-                            <li><a class="dropdown-item" href="{{route('role.index')}}">Role</a></li>
+                            @if(auth()->user()->role_id == '1')
+                                <li><a class="dropdown-item" href="{{route('role.index')}}">Role</a></li>
+                                <li><a class="dropdown-item" href="{{route('fakultas.index')}}">Fakultas</a></li>
+                                <li><a class="dropdown-item" href="{{route('programStudi.index')}}">Program Studi</a></li>
+                                <li><a class="dropdown-item" href="{{route('jenisBeasiswa.index')}}">Jenis Beasiswa</a></li>
+                                <li><a class="dropdown-item" href="{{route('users.index')}}">Users</a></li>
+                            @endif
+                            @if(auth()->user()->role_id == '3' or auth()->user()->role_id == '2' or auth()->user()->role_id == '4')
+                                <li><a class="dropdown-item" href="{{route('dokumenBeasiswa.index')}}">Hasil Pengajuan</a></li>
+                                    @if(auth()->user()->role_id == '3' or auth()->user()->role_id == '2')
+                                        <li><a class="dropdown-item" href="{{route('tanggalPeriode.index')}}">Tanggal Periode Beasiswa</a></li>
+                                    @endif
+                            @endif
                         </ul>
                     </li>
                         @if(auth()->user()->role_id == '1')
